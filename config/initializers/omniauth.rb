3950f49768c81590abbcf6d35b8438b7167177ac
KT <truongmkevin@yahoo.com>
2018-04-01 18:12:48 +0000

    initial commit; added instagram omniauth, continue on from slides for GitHub and styling

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET'], scope:['basic']
+end
